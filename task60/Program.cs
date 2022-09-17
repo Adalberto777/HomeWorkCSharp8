63797509002c01991414ddb93309a52bfab4ec9f
Adalberto777 <albert.tliashev@gmail.com>
2022-09-17 20:55:14 +0500

    end17092022

--- task60/Program.cs
+++ task60/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы
-int[,,] GetMatrix3(int pages, int lines, int columns) //метод с параметрами: размер, мин элемент, макс элемент создает массив 
-{                                                                   //заполненный случайными числами
+int[,,] GetMatrix3(int pages, int lines, int columns) 
+{                                                                   
     int[,,] result = new int[pages, lines, columns];
     int count = 10; 
 
@@ -11,14 +11,14 @@ int[,,] GetMatrix3(int pages, int lines, int columns) //метод с парам
             for (int k = 0; k < result.GetLength(2); k++)
             {
                 result[i, j, k] = count;
-                count += 1;
+                count ++;
             }
         }
     }
     return result;
 }
 
-void PrintMatrix3(int[,,] matrix31) //метод распечатывает двумерный массив
+void PrintMatrix3(int[,,] matrix31) 
 {
     for (int i = 0; i < matrix31.GetLength(0); i++)
     {
@@ -33,9 +33,7 @@ void PrintMatrix3(int[,,] matrix31) //метод распечатывает дв
     }
 }
 
-int[,,] matrix3 = GetMatrix3(2, 2, 2); //1-страницы 2-строки 3-столбцы 4-минимальное число 5-максимальное число
-
-
+int[,,] matrix3 = GetMatrix3(2, 2, 2); //1-страницы 2-строки 3-столбцы
 
 PrintMatrix3(matrix3);
 
